009c5933230e84e88e8becb8824a16a8184ff11c
Joel <121259754+Jooell1@users.noreply.github.com>
2023-02-17 19:35:09 -0300

    Update nv-Que mira bobo _mp3.js

--- plugins/nv-Que mira bobo _mp3.js
+++ plugins/nv-Que mira bobo _mp3.js
@@ -3,8 +3,8 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/Que mira bobo.mp3'
-conn.sendFile(m.chat, vn, 'Que mira bobo.mp3', null, m, true, {
+let vn = './media/Que mira bobo.mp4'
+conn.sendFile(m.chat, vn, 'Que mira bobo.mp4', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
