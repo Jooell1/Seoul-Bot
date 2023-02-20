3a120361796f5d338fc39d840b872f18696c1e5f
Joel <121259754+Jooell1@users.noreply.github.com>
2023-02-20 14:12:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -229,6 +229,19 @@ let [ffmpeg, ffprobe, ffmpegWebp, convert, magick, gm, find] = test
 let s = global.support = { ffmpeg, ffprobe, ffmpegWebp, convert, magick, gm, find }
 Object.freeze(global.support)
 }
+if (stopped == 'close') return        
+const status = global.db.data.settings[conn.user.jid] || {}
+let _uptime = process.uptime() * 1000    
+let uptime = clockString(_uptime)
+let bio = `🤖 ᴛɪᴇᴍᴘᴏ ᴀᴄᴛɪᴠᴏ: ${uptime} ┃ 👑 ʙʏ ᴍᴀꜰᴜʏᴜ-ʙᴏᴛ`
+await conn.updateProfileStatus(bio).catch(_ => _)
+}, 60000)
+function clockString(ms) {
+let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [d, ' Día(s) ️', h, ' Hora(s) ', m, ' Minuto(s) ', s, ' Segundo(s) '].map(v => v.toString().padStart(2, 0)).join('')}
 _quickTest()
 .then(() => conn.logger.info(`Ƈᴀʀɢᴀɴᴅᴏ..\n`))
 .catch(console.error)
