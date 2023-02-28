9258b8768cd85105c9d2875b65d78220102c0225
Joel <121259754+Jooell1@users.noreply.github.com>
2023-02-28 16:46:57 -0300

    Update _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -4,7 +4,7 @@ export async function before(m, { isAdmin, isBotAdmin }) {
  
 let chat = db.data.chats[m.chat]
 if (/^[.~#/\$,](read)?viewonce/.test(m.text)) return
-if (!chat.antiver || chat.isBanned) return
+if (!chat.antiviewonce || chat.isBanned) return
 if (m.mtype == 'viewOnceMessageV2') {
 let msg = m.message.viewOnceMessageV2.message
 let type = Object.keys(msg)[0]
@@ -13,7 +13,7 @@ let buffer = Buffer.from([])
 for await (const chunk of media) {
 buffer = Buffer.concat([buffer, chunk])}
 if (/video/.test(type)) {
-return this.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption}\n\n*𝑯𝒆𝒚 𝒂𝒒𝒖𝒊́ 𝒏𝒐 𝒔𝒆 𝒑𝒆𝒓𝒎𝒊𝒕𝒆 𝒐𝒄𝒖𝒍𝒕𝒂𝒓 𝒏𝒂𝒅𝒂 𝒓𝒂𝒕𝒂 😎!!*`, m)
+return this.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption}\n\n*𝙰𝚀𝚄𝙸 𝙽𝙾 𝚂𝙴 𝙿𝙴𝚁𝙼𝙸𝚃𝙴 𝙾𝙲𝚄𝙻𝚃𝙰𝚁 𝙽𝙰𝙳𝙰*`, m)
 } else if (/image/.test(type)) {
-return this.sendFile(m.chat, buffer, 'error.jpg', `${msg[type].caption}\n\n*𝑯𝒆𝒚 𝒂𝒒𝒖𝒊́ 𝒏𝒐 𝒔𝒆 𝒑𝒆𝒓𝒎𝒊𝒕𝒆 𝒐𝒄𝒖𝒍𝒕𝒂𝒓 𝒏𝒂𝒅𝒂 𝒓𝒂𝒕𝒂 😎!!*`, m)
+return this.sendFile(m.chat, buffer, 'error.jpg', `${msg[type].caption}\n\n*𝙰𝚀𝚄𝙸 𝙽𝙾 𝚂𝙴 𝙿𝙴𝚁𝙼𝙸𝚃𝙴 𝙾𝙲𝚄𝙻𝚃𝙰𝚁 𝙽𝙰𝙳𝙰*`, m)
 }}}
